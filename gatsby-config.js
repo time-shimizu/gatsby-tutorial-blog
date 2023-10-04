1a1320bfa39127369915ce43f53b8c8d7475b47b
time-shimizu <213.f.r48132@gmail.com>
2023-10-04 23:25:51 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GatsbyTutorialBlog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
