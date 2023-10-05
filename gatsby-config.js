b3d194071d12fcd582ceb2661e81a6be0d78c0de
time-shimizu <213.f.r48132@gmail.com>
2023-10-05 23:12:23 +0900

    finish part3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `GatsbyTutorialBlog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
